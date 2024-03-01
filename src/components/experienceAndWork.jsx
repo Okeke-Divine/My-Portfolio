8a7fb74d3b5afe3558ddfad7a85d8d40b0b22b2e
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-01 15:00:34 +0100

    Major update/rebrand

---
+++ src/components/experienceAndWork.jsx
@@ -0,0 +1,45 @@
+export default function ExperienceAndWork() {
+  return (
+    <section className="experienceANDwork jnUoFC" id="experienceANDwork">
+      <div className="sectionTitle">
+        <div className="text-primary font-mono">03.</div>
+        <div className="font-mono">Where I’ve Worked</div>
+        <div className="horiLine">
+          <div className="horiLineMain"></div>
+        </div>
+      </div>
+      <div className="eAw">
+        <div className="left">
+          <div className="active">Marokug</div>
+        </div>
+        <div className="right">
+          <div className="highlitedTabTitle">
+            Senior Full Stack Web Dev @
+            <a href="https://marokug.vercel.app/" style={{ 'margin-left': '5px' }} target="_blank" className="text-primary">
+              Marokug
+            </a>
+          </div>
+          <div className="highlitedTabSubTitle">
+            January 2024 - Present
+          </div>
+          <div className="highlitedTabContent">
+            <ul>
+              <li>
+                <b>Spearheaded the development of Marokug, an educational platform empowering students to learn courses, participate in quizzes and exams, and explore various learning materials.</b> Utilized a modern tech stack including React, Tailwind CSS, Django, and MySQL to deliver a user-friendly and scalable web application.
+              </li>
+              <li>
+                <b>Architected and implemented robust back-end systems for various projects, leveraging Django's robust framework and MySQL's efficient database management capabilities.</b> Ensured data integrity, security, and scalability to meet the evolving needs of the applications.
+              </li>
+              <li>
+                <b>Collaborated effectively with cross-functional teams, including designers and front-end developers, to translate design mockups into functional and responsive user interfaces using React and Tailwind CSS.</b> Delivered pixel-perfect experiences that exceeded user expectations.
+              </li>
+              <li>
+                <b>Played a key role in troubleshooting and resolving complex technical issues, ensuring the smooth operation and performance of multiple web applications.</b> Maintained a deep understanding of various technologies to identify and rectify issues efficiently.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+} 
---
+++ src/components/footer.jsx
@@ -0,0 +1,25 @@
+export default function Footer(props) {
+  const socialMedia = props.socialMedia
+
+  return (
+    <section className="footer jnUoFC">
+      Designed by{" "}
+      <a
+        href={socialMedia.originalCreatorBchiang}
+        target="_blank"
+        className="designNdInspiredByLink"
+      >
+        Brittany Chiang <i className="fa fa-external-link fj93j9r"></i>
+      </a>{" "}
+      & Rebuilt by{" "}
+      <a
+        href={socialMedia.mySocials.github}
+        target="_blank"
+        className="designNdInspiredByLink"
+      >
+        Okeke Divine-Vessel{" "}
+        <i className="fa fa-external-link fj93j9r"></i>
+      </a>
+    </section>
+  )
+}
