cb5b98ff6b286498ae5ce54c7334245e9216c8e2
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2025-09-16 02:43:25 +0200

    added project image slideshow

--- src/components/projectItemComponent.jsx
+++ src/components/projectItemComponent.jsx
@@ -1,27 +1,91 @@
+import React from 'react'
+
 export default function ProjectItemComponent(props) {
+  const openLinkInNewTab = props.openLinkInNewTab;
+  const projectDirection = props.projectDirection;
+  const projectTitle = props.projectTitle;
+  const projectDescription = props.projectDescription;
+  const projectTags = props.projectTags;
+  const projectLinks = props.projectLinks;
+  const projectCoverImage = props.projectCoverImage;
+  const projectSlideShowImages = props.projectSlideShowImages;
 
-  const openLinkInNewTab = props.openLinkInNewTab
+  // Combine cover image with slideshow images if available
+  const allImages = [projectCoverImage, ...(projectSlideShowImages || [])].filter(Boolean);
+  const hasMultipleImages = allImages.length > 1;
 
-  const projectDirection = props.projectDirection
-  const projectTitle = props.projectTitle
-  const projectDescription = props.projectDescription
-  const projectTags = props.projectTags
-  const projectLinks = props.projectLinks
-  const projectCoverImage = props.projectCoverImage
-  const projectSlideShowImages = props.projectSlideShowImages
+  // State for slideshow
+  const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
+  const [isSlideshowPlaying, setIsSlideshowPlaying] = React.useState(hasMultipleImages);
+  const [isFullscreen, setIsFullscreen] = React.useState(false);
 
-  const projectTagsComponents = []
+  // Auto-advance slideshow
+  React.useEffect(() => {
+    if (!hasMultipleImages || !isSlideshowPlaying || isFullscreen) return;
 
-  const projectLinkComponents = Object.keys(projectLinks).map(link => {
-    if (link === 'github' && projectLinks[link] !== null) {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % allImages.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [hasMultipleImages, isSlideshowPlaying, isFullscreen, allImages.length]);
+
+  // Handle fullscreen mode
+  const openFullscreen = () => {
+    setIsFullscreen(true);
+    setIsSlideshowPlaying(false);
+  };
+
+  const closeFullscreen = () => {
+    setIsFullscreen(false);
+    if (hasMultipleImages) {
+      setIsSlideshowPlaying(true);
+    }
+  };
+
+  // Navigate images
+  const goToNextImage = (e) => {
+    e && e.stopPropagation();
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % allImages.length);
+  };
+
+  const goToPrevImage = (e) => {
+    e && e.stopPropagation();
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + allImages.length) % allImages.length);
+  };
+
+  // Project tags components
+  const projectTagsComponents = projectTags.map((tag, index) => (
+    <div key={index} className="projectTags otherProjectTags">
+      {tag}
+    </div>
+  ));
+
+  // Project link components
+  const projectLinkComponents = Object.keys(projectLinks).map((link) => {
+    if (link === "github" && projectLinks[link] !== null) {
       return (
-        <div key={link} className="projectLinkIcon" onClick={(e) => openLinkInNewTab(projectLinks[link])}>
+        <div
+          key={link}
+          className="projectLinkIcon"
+          onClick={(e) => {
+            e.stopPropagation();
+            openLinkInNewTab(projectLinks[link]);
+          }}
+        >
           <i className="fa fa-github"></i>
         </div>
       );
-    } else if (link === 'livePreview' && projectLinks[link] !== null) {
+    } else if (link === "livePreview" && projectLinks[link] !== null) {
       return (
-        <div key={link} className="projectLinkIcon" onClick={(e) => openLinkInNewTab(projectLinks[link])}>
+        <div
+          key={link}
+          className="projectLinkIcon"
+          onClick={(e) => {
+            e.stopPropagation();
+            openLinkInNewTab(projectLinks[link]);
+          }}
+        >
           <i className="fa fa-external-link"></i>
         </div>
       );
@@ -29,83 +93,123 @@ export default function ProjectItemComponent(props) {
     return null;
   });
 
-  // console.log(projectCoverImage)
+  // Slideshow controls component
+  const slideshowControls = hasMultipleImages && (
+    <div className="slideshow-controls">
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          goToPrevImage();
+        }}
+        className="slideshow-nav-btn"
+      >
+        <i className="fa fa-chevron-left"></i>
+      </button>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsSlideshowPlaying(!isSlideshowPlaying);
+        }}
+        className="slideshow-play-pause"
+      >
+        <i className={`fa ${isSlideshowPlaying ? "fa-pause" : "fa-play"}`}></i>
+      </button>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          goToNextImage();
+        }}
+        className="slideshow-nav-btn"
+      >
+        <i className="fa fa-chevron-right"></i>
+      </button>
+    </div>
+  );
 
-  const projectCoverImageComponent = [];
-  if (projectCoverImage != null) {
-    projectCoverImageComponent.push(
-      <img key={projectTitle + " Project Image"} src={projectCoverImage} alt="Project Image" className="project-image" />
-    )
-  }
+  // Fullscreen modal
+  const fullscreenModal = isFullscreen && (
+    <div className="fullscreen-modal" onClick={closeFullscreen}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="close-modal" onClick={closeFullscreen}>
+          <i className="fa fa-times"></i>
+        </button>
+        <img
+          src={allImages[currentImageIndex]}
+          alt="Project in full screen"
+          className="fullscreen-image"
+        />
+        {hasMultipleImages && (
+          <div className="modal-controls">
+            <button onClick={goToPrevImage} className="modal-nav-btn">
+              <i className="fa fa-chevron-left"></i>
+            </button>
+            <div className="image-counter">
+              {currentImageIndex + 1} / {allImages.length}
+            </div>
+            <button onClick={goToNextImage} className="modal-nav-btn">
+              <i className="fa fa-chevron-right"></i>
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 
+  // Image display with slideshow capability
+  const projectImageComponent = (
+    <div className="projectImage" onClick={openFullscreen}>
+      <img
+        src={allImages[currentImageIndex]}
+        alt="Project Image"
+        className="project-image"
+      />
+      {hasMultipleImages && (
+        <div className="slideshow-indicator">
+          <div className="slideshow-dots">
+            {allImages.map((_, index) => (
+              <span
+                key={index}
+                className={`dot ${index === currentImageIndex ? "active" : ""}`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setCurrentImageIndex(index);
+                }}
+              ></span>
+            ))}
+          </div>
+        </div>
+      )}
+      {slideshowControls}
+    </div>
+  );
 
-  projectTags.forEach((tag, index) => {
-    projectTagsComponents.push(<div key={index} className="projectTags otherProjectTags">{tag}</div>)
-  })
-
-
-
-
-  // conditonal rendering
+  // Conditional rendering based on project direction
   return (
     <div>
-      {projectDirection === 'left' ? (
+      {fullscreenModal}
+      {projectDirection === "left" ? (
         <div className="projectContainer projectContainerLayout1">
-          <div className="left">
-            <div className="projectImage">
-              {projectCoverImageComponent}
-            </div>
-          </div>
+          <div className="left">{projectImageComponent}</div>
           <div className="right">
-            <div className="featuredProject text-primary">
-              Featured Project
-            </div>
+            <div className="featuredProject text-primary">Featured Project</div>
             <div className="projectName">{projectTitle}</div>
-            <div className="projectInfo">
-              {projectDescription}
-            </div>
-            <div className="projectsTags">
-              {projectTagsComponents}
-            </div>
-            {projectSlideShowImages !== null ?
-              (<div className="projectSlideshowCTA">
-                <button onClick={() => alert('This feature is under development.')}>Image Slideshow <i className="fa fa-angle-double-right"></i></button>
-              </div>)
-              : null}
-            <div className="projectsIcons">
-              {projectLinkComponents}
-            </div>
+            <div className="projectInfo">{projectDescription}</div>
+            <div className="projectsTags">{projectTagsComponents}</div>
+            <div className="projectsIcons">{projectLinkComponents}</div>
           </div>
         </div>
       ) : (
         <div className="projectContainer projectContainerLayout2">
           <div className="right">
-            <div className="featuredProject text-primary">
-              Featured Project
-            </div>
+            <div className="featuredProject text-primary">Featured Project</div>
             <div className="projectName">{projectTitle}</div>
-            <div className="projectInfo">
-              {projectDescription}
-            </div>
-            <div className="projectsTags">
-              {projectTagsComponents}
-            </div>
-            {projectSlideShowImages !== null ?
-              (<div className="projectSlideshowCTA">
-                <button onClick={() => alert('This feature is under development.')}>Image Slideshow <i className="fa fa-angle-double-right"></i></button>
-              </div>)
-              : null}
-            <div className="projectsIcons">
-              {projectLinkComponents}
-            </div>
+            <div className="projectInfo">{projectDescription}</div>
+            <div className="projectsTags">{projectTagsComponents}</div>
+            <div className="projectsIcons">{projectLinkComponents}</div>
           </div>
-          <div className="left">
-            <div className="projectImage">
-              {projectCoverImageComponent}
-            </div>
-          </div>
+          <div className="left">{projectImageComponent}</div>
         </div>
       )}
     </div>
-  )
+  );
 }
