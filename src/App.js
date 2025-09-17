0b187509dc8c367bced30fc2dfc04d63253e6653
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2025-09-17 14:59:30 +0200

    Updated the scrollbar styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./assets/css/css-responsive-max-width-800.css";
 import "./assets/css/css-responsive-max-width-1310.css";
 import "./assets/css/css-mW-620.css";
 import "./assets/css/main.css";
+import "./assets/css/scrollbar.css";
 import myLogo from "./assets/images/myLogo.png";
 import socialMedia from "./data/socialMedia.json";
 import openLinkInNewTab from "./utils/openLinkInNewTab.js";
